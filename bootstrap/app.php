8c8040514e42b6242465a3bd910f729dba0cd83d
Alejandro Garcia <jandro_es@hotmail.com>
2024-06-05 14:06:40 +0200

    mejoras en admin layouts

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         then: function (){
             Route::middleware('auth:web')
             ->prefix('admin')
+            ->name('admin.')
             ->group(base_path('routes/admin.php'));
         }
     )
