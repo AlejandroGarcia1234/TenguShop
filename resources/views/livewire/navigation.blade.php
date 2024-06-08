f5eab90de0d6f06815a7ceca712cb39c2fca6424
Alejandro Garcia <jandro_es@hotmail.com>
2024-06-08 21:29:09 +0200

    avances portada

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,9 +1,11 @@
-<div>
+<div x-data="{
+    open:false,
+}">
     <header class="bg-purple-600">
         <x-container class="px-4 py-4">
             <div class="flex justify-between items-center space-x-8">
 
-                <button class="text-xl md:text-3xl">
+                <button class="text-xl md:text-3xl" x-on:click="open = true">
                     <i class="fas fa-bars text-white"></i>
                 </button>
 
@@ -21,10 +23,66 @@
                     <x-input class="w-full" placeholder="Buscar por producto" />
                 </div>
 
-                <div class=" flex items-center space-x-4 md:space-x-8">
-                    <button class="text-xl md:text-3xl">
-                        <i class="fas fa-user text-white"></i>
-                    </button>
+                <div class="flex items-center space-x-4 md:space-x-8">
+                    
+                    <x-dropdown>
+
+                        <x-slot name="trigger">
+
+                            @auth
+
+                            <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                            </button>
+
+                            @else
+
+                            <button class="text-xl md:text-3xl">
+                                <i class="fas fa-user text-white"></i>
+                            </button>
+
+                            @endauth
+
+                           
+                        </x-slot>
+
+                        <x-slot name="content">
+                            @guest
+                            <div class="px-4 py-2">
+                                <div class="flex justify-center">
+                                    <a href="{{route('login')}}" class="btn btn-purple">
+                                        Iniciar sesión
+                                    </a>
+                                </div>
+                                <p class="text-sm text-center mt-2">
+                                    ¿No tienes cuenta? <a href="{{route('register')}}" class="text-purple-600 hover:underline">Regístrate</a>
+                                </p>
+                            </div>
+
+                            @else
+
+                            <x-dropdown-link href="{{route('profile.show')}}">
+                                Mi perfil
+                            </x-dropdowm-link>
+
+                            <div class="border-t border-gray-200"></div>
+
+                            <form method="POST" action="{{ route('logout') }}" x-data>
+                                @csrf
+
+                                <x-dropdown-link href="{{ route('logout') }}"
+                                         @click.prevent="$root.submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+
+                            @endguest
+                            
+                        </x-slot>
+
+                    </x-dropdown>
+                    
+                    
                     <button class="text-xl md:text-3xl">
                         <i class="fas fa-shopping-cart text-white"></i>
                     </button>
@@ -38,11 +96,11 @@
         </x-container>
     </header>
 
-    <div class="fixed top-0 left-0 inset-0 bg-black bg-opacity-25 z-10">
+    <div x-show="open" x-on:click="open = false" style="display:none" class="fixed top-0 left-0 inset-0 bg-black bg-opacity-25 z-10">
 
     </div>
 
-    <div class="fixed top-0 left-0 z-20">
+    <div x-show="open" style="display:none" class="fixed top-0 left-0 z-20">
 
         <div class="flex">
 
@@ -54,7 +112,7 @@
                             Hola
                         </span>
 
-                        <button>
+                        <button x-on:click="open = false">
                             <i class="fas fa-times"></i>
                         </button>
                     </div>
