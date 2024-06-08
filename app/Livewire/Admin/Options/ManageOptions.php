e7b28a69b4e2e51303ea02b216c296d0bf3cd762
Alejandro Garcia <jandro_es@hotmail.com>
2024-06-08 02:26:42 +0200

    cambios en opciones

--- app/Livewire/Admin/Options/ManageOptions.php
+++ app/Livewire/Admin/Options/ManageOptions.php
@@ -2,17 +2,67 @@
 
 namespace App\Livewire\Admin\Options;
 
+use App\Livewire\Forms\Admin\Options\NewOptionForm;
+use App\Models\Feature;
 use Livewire\Component;
 use App\Models\Option;
+use Livewire\Attributes\On;
+
 
 class ManageOptions extends Component
 {
 
     public $options;
 
+    public  NewOptionForm $newOption;
+
+
     public function mount()
     {
         $this->options = Option::with('features')->get();
+    }
+
+    #[On('featureAdded')]
+    public function updateOptionList()
+    {
+        $this->options = Option::with('features')->get();
+    }
+
+    public function addFeature()
+    {
+       $this->newOption->addFeature();
+    }
+
+    public function removeFeature($index)
+    {
+        $this->newOption->removeFeature($index);
+    }
+
+    public function deleteFeature(Feature $feature)
+    {
+        $feature->delete();
+        $this->options = Option::with('features')->get();
+    }
+
+    public function deleteOption(Option $option)
+    {
+        $option->delete();
+        $this->options = Option::with('features')->get();
+    }
+
+    public function addOption()
+    {
+
+            $this->newOption->save();
+
+            $this->options = Option::with('features')->get();
+
+            $this->dispatch('swal', [
+                'icon' => 'success',
+                'title' => '¡Buen trabajo!',
+                'text' => 'La opción se ha agregado correctamente',
+            ]);
+        
     }
 
     public function render()
